8c9264a026d0f607332ac08d4131089c1ea50147
MarilynSuquillo <marilyn.suquillo@gmail.com>
2023-08-01 22:16:29 -0500

    filtré la data a 9 variables iniciales

--- Script/CódigoGrupo3.R
+++ Script/CódigoGrupo3.R
@@ -8,3 +8,7 @@ library(ggplot2)
 empresas<-read.xlsx("Datos/balances_2014.xlsx")
 #Convertir a tibble
 EMPRESAS_T <-tibble:: as_tibble(empresas)
+empresas_FIL <- EMPRESAS_T %>% 
+    select(Empresas = nombre_cia, Status = situacion, Tpo_de_empresa = tipo, País = pais,
+         Provincia = provincia, Cantón = canton, Ciudad = ciudad, Actividad_económica = ciiu4_nivel1, Subactividad = ciiu4_nivel6) %>%  view("data_filtrada")
+  
